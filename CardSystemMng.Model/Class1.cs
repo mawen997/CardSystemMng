a7030709194fd0c64d7b841c738b230c9c5f7845
马光文 <beijing2008h@126.com>
2020-03-14 00:01:58 +0800

    创建Model，并添加数据链接，通过sqlsugar自动生成数据表实体类

--- CardSystemMng.Model/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CardSystemMng.Model
-{
-    public class Class1
-    {
-    }
-}
---
+++ CardSystemMng.Model/SeedDt/FrameSeedMd.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardSystemMng.Model.SeedDt
+{
+    public class FrameSeedMd
+    { /// <summary>
+      /// 生成Model层
+      /// </summary>
+      /// <param name="myContext"></param>
+      /// <returns></returns>
+        public static bool CreateModels(MyContext myContext)
+        {
+
+            try
+            {
+                myContext.Create_Model_ClassFileByDBTalbe($@"E:\实验室\ASP.NET COR学习\CardSystemMng\CardSystemMng.Model\Models", "CardSystemMng.Model.Models", new string[] { }, "");
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// 生成IRepository层
+        /// </summary>
+        /// <param name="myContext"></param>
+        /// <returns></returns>
+        public static bool CreateIRepositorys(MyContext myContext)
+        {
+
+            try
+            {
+                myContext.Create_IRepository_ClassFileByDBTalbe($@"E:\实验室\ASP.NET COR学习\CardSystemMng\CardSystemMng.IRepository", "CardSystemMng.IRepository", new string[] { }, "");
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// 生成 IService 层
+        /// </summary>
+        /// <param name="myContext"></param>
+        /// <returns></returns>
+        public static bool CreateIServices(MyContext myContext)
+        {
+
+            try
+            {
+                myContext.Create_IServices_ClassFileByDBTalbe($@"E:\实验室\ASP.NET COR学习\CardSystemMng\CardSystemMng.IServices", "CardSystemMng.IServices", new string[] { "Module" }, "");
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// 生成 Repository 层
+        /// </summary>
+        /// <param name="myContext"></param>
+        /// <returns></returns>
+        public static bool CreateRepository(MyContext myContext)
+        {
+
+            try
+            {
+                myContext.Create_Repository_ClassFileByDBTalbe($@"E:\实验室\ASP.NET COR学习\CardSystemMng\CardSystemMng.Repository", "CardSystemMng.Repository", new string[] { "Module" }, "");
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// 生成 Service 层
+        /// </summary>
+        /// <param name="myContext"></param>
+        /// <returns></returns>
+        public static bool CreateServices(MyContext myContext)
+        {
+
+            try
+            {
+                myContext.Create_Services_ClassFileByDBTalbe($@"E:\实验室\ASP.NET COR学习\CardSystemMng\CardSystemMng.Services", "CardSystemMng.Services", new string[] { "Module" }, "");
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+    }
+}
